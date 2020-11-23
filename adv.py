bec1207294580ab0494e094cbc39143dfc04612c
ezorigo <bzorigo@gmail.com>
2020-11-22 22:41:32 -0500

    mvp

--- adv.py
+++ adv.py
@@ -27,9 +27,29 @@ player = Player(world.starting_room)
 
 # Fill this out with directions to walk
 # traversal_path = ['n', 'n']
-traversal_path = []
 
+def maze_traversal(current_room, visited=None):
+    path = []
+    reverse = {'n': 's', 'e': 'w', 's': 'n', 'w': 'e'}
 
+    if visited == None:
+        visited = set()
+
+    for move in player.current_room.get_exits():
+        player.travel(move)
+
+        if player.current_room in visited:
+            player.travel(reverse[move])
+        else:
+            visited.add(player.current_room)
+            path.append(move)
+            path = path + maze_traversal(player.current_room, visited)
+            player.travel(reverse[move])
+            path.append(reverse[move])
+
+    return path
+
+traversal_path = maze_traversal(player.current_room)
 
 # TRAVERSAL TEST - DO NOT MODIFY
 visited_rooms = set()
@@ -50,13 +70,13 @@ else:
 
 #######
 # UNCOMMENT TO WALK AROUND
-#######
-player.current_room.print_room_description(player)
-while True:
-    cmds = input("-> ").lower().split(" ")
-    if cmds[0] in ["n", "s", "e", "w"]:
-        player.travel(cmds[0], True)
-    elif cmds[0] == "q":
-        break
-    else:
-        print("I did not understand that command.")
+# #######
+# player.current_room.print_room_description(player)
+# while True:
+#     cmds = input("-> ").lower().split(" ")
+#     if cmds[0] in ["n", "s", "e", "w"]:
+#         player.travel(cmds[0], True)
+#     elif cmds[0] == "q":
+#         break
+#     else:
+#         print("I did not understand that command.")
